4f4709085b0840b530dd14ed08093f35af692a12
David R <davrodzam@gmail.com>
2023-05-27 14:01:24 -0600

    Design #3 Icons - Title

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -6,18 +6,30 @@ class CardTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Table(
-      children: [
+      children: const [
         TableRow(children: [
-          _SingleCard(),
-          _SingleCard(),
+          _SingleCard(
+              icon: Icons.border_all, text: 'General', color: Colors.amber),
+          _SingleCard(
+              icon: Icons.flight_outlined,
+              text: 'Tickets',
+              color: Colors.amber),
         ]),
         TableRow(children: [
-          _SingleCard(),
-          _SingleCard(),
+          _SingleCard(
+              icon: Icons.android, text: 'Android', color: Colors.amber),
+          _SingleCard(icon: Icons.cabin, text: 'Hotel', color: Colors.amber),
         ]),
         TableRow(children: [
-          _SingleCard(),
-          _SingleCard(),
+          _SingleCard(
+              icon: Icons.pedal_bike, text: 'Ciclismo', color: Colors.blue),
+          _SingleCard(
+              icon: Icons.local_pizza, text: 'Pizza', color: Colors.red),
+        ]),
+        TableRow(children: [
+          _SingleCard(
+              icon: Icons.shopping_cart, text: 'Shopping', color: Colors.blue),
+          _SingleCard(icon: Icons.bus_alert, text: 'Bus', color: Colors.red),
         ]),
       ],
     );
@@ -25,25 +37,36 @@ class CardTable extends StatelessWidget {
 }
 
 class _SingleCard extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final String text;
+
+  const _SingleCard({
+    super.key,
+    required this.icon,
+    required this.color,
+    required this.text,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
       height: 180,
       decoration: const BoxDecoration(color: Color.fromRGBO(62, 66, 107, 0.7)),
-      child: const Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           CircleAvatar(
             backgroundColor: Colors.amber,
-            child: Icon(Icons.pie_chart, size: 35),
+            child: Icon(icon),
           ),
-          SizedBox(
+          const SizedBox(
             height: 10,
           ),
           Text(
-            'General',
-            style: TextStyle(color: Colors.amber, fontSize: 20),
+            text,
+            style: TextStyle(color: color, fontSize: 20),
           )
         ],
       ),
